ad721aa2a942a4bbed73335fa0e6cc57aea5a8da
na181286 <na181286@gmail.com>
2020-09-03 20:34:14 +0300

    latest

---
+++ README.md
@@ -0,0 +1,11 @@
+To run the app, run these commands in two separate terminal windows from the root:
+
+```bash
+cd spaceX/server && npm install && npm start
+```
+
+and
+
+```bash
+cd spaceX/client && npm install && npm start
+```
---
+++ server/src/index.js
@@ -0,0 +1,67 @@
+const {ApolloServer}   = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+const isEmail = require('isemail');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+  const server = new ApolloServer({
+    context: async ({ req }) => {
+      // simple auth check on every request
+      const auth = req.headers && req.headers.authorization || '';
+      const email = Buffer.from(auth, 'base64').toString('ascii');
+      if (!isEmail.validate(email)) return { user: null };
+      // find a user by their email
+      const users = await store.users.findOrCreate({ where: { email } });
+      const user = users && users[0] || null;
+      return { user: { ...user.dataValues } };
+    },
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+      launchAPI: new LaunchAPI(),
+      userAPI: new UserAPI({ store })
+    })
+  });
+  
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+
+  //Examples of queries:
+  // query GetLaunches {
+  //   launches {
+  //     id
+  //     mission {
+  //       name
+  //     }
+  //   }
+  // }
+
+  // query GetLaunchById {
+  //   launch(id: 60) {
+  //     id
+  //     rocket {
+  //       id
+  //       type
+  //     }
+  //   }
+  //}
+
+
+  // query GetLaunchById($id: ID!) {
+  //   launch(id: $id) {
+  //     id
+  //     rocket {
+  //       id
+  //       type
+  //     }
+  //   }
+  // }
+
+  // {
+  //   "id": 60
+  // }
